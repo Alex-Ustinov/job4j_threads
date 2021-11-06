c4c5e12de34742aff278ebdf14a752629ecc42fc
18485888 <AAlUstinov@sberbank.ru>
2021-11-07 01:53:11 +0300

    Состояние нити

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        first.start();
+        second.start();
+        Boolean flag = true;
+        while (flag) {
+            if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
+                System.out.println("Both threads were finished");
+                flag = false;
+            }
+        }
+    }
+}
