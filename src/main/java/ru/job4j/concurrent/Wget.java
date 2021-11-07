8ebb84d23c1b54a3f0ee803b3c71f604c4728a81
18485888 <AAlUstinov@sberbank.ru>
2021-11-07 12:24:44 +0300

    Режим ожидания

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println("Start loading ... ");
+                for (int index = 0; index <= 100; index++) {
+                    Thread.sleep(index * 1000);
+                    System.out.print("\rLoading : " + index  + "%");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+}
