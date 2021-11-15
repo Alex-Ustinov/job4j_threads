48e4778b6700269cf2f5ff9ed89c1495dc1a5a2b
18485888 <AAlUstinov@sberbank.ru>
2021-11-15 19:27:26 +0300

    Immutable объекты

--- src/main/java/ru/job4j/thread/Node.java
+++ src/main/java/ru/job4j/thread/Node.java
@@ -1,12 +1,10 @@
 package ru.job4j.thread;
 
 public class Node<T> {
-    private Node<T> next;
-    private T value;
+    private final Node<T> next;
+    private final T value;
 
-    Node () {}
-
-    Node (Node<T> next, T value) {
+    Node(Node<T> next, T value) {
         this.next = next;
         this.value = value;
     }
