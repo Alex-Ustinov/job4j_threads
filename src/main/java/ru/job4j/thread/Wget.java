336497e52f898a986f563eb5d7df0c8965677869
18485888 <AAlUstinov@sberbank.ru>
2021-11-15 12:53:50 +0300

    Скачивание файла с ограничением

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,5 +1,11 @@
 package ru.job4j.thread;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Timer;
+
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
@@ -8,10 +14,27 @@ public class Wget implements Runnable {
         this.url = url;
         this.speed = speed;
     }
-
     @Override
     public void run() {
-        /* Скачать файл*/
+        long startTime = System.nanoTime();
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+            long estimatedTime = System.nanoTime() - startTime;
+            int spentTime = (int) estimatedTime  / 1000000;
+            int waitTime = speed - spentTime;
+            System.out.println("spentTime = " + spentTime);
+            System.out.println("waitTime = " + waitTime);
+            Thread.sleep(waitTime);
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+        System.out.println("The file was downloaded");
     }
 
     public static void main(String[] args) throws InterruptedException {
