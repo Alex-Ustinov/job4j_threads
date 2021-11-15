d64e7c15a97bba630ae220896de9e20f617d1e30
18485888 <AAlUstinov@sberbank.ru>
2021-11-15 19:01:45 +0300

    Thread без общих ресурсов

---
+++ src/main/java/ru/job4j/thread/UserCache.java
@@ -0,0 +1,35 @@
+package ru.job4j.thread;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public ConcurrentHashMap<Integer, User> getUsers() {
+        return users;
+    }
+
+    public List<User> findAll() {
+        ArrayList<User> result = new ArrayList<>();
+        for (Map.Entry<Integer, User> user : users.entrySet()) {
+            result.add(User.of(user.getValue().getName()));
+        }
+        return result;
+    }
+}
