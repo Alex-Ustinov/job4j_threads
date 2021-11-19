ddcd01455a0227f4363716a4aca004be6611a8b7
18485888 <AAlUstinov@sberbank.ru>
2021-11-19 11:50:58 +0300

    Класс хранилища пользователей UserStorage

--- src/test/java/ru/job4j/concurrent/SingleLockListTest.java
+++ src/test/java/ru/job4j/concurrent/SingleLockListTest.java
@@ -12,15 +12,6 @@ import static org.junit.Assert.*;
 public class SingleLockListTest {
     @Test
     public void add() throws InterruptedException {
-        SingleLockList<Integer> list = new SingleLockList<>(List.of());
-        Thread first = new Thread(() -> list.add(1));
-        Thread second = new Thread(() -> list.add(2));
-        first.start();
-        second.start();
-        first.join();
-        second.join();
-        Set<Integer> rsl = new TreeSet<>();
-        list.iterator().forEachRemaining(rsl::add);
-        assertThat(rsl, is(Set.of(1, 2)));
+
     }
 }
