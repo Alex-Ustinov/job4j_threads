2d540658039bb5901274c60a0f08a90a5b2a80ab
18485888 <AAlUstinov@sberbank.ru>
2021-11-22 16:35:09 +0300

    Producer Consumer

---
+++ src/main/java/ru/job4j/thread/SimpleBlockingQueue.java
@@ -0,0 +1,54 @@
+package ru.job4j.thread;
+
+import net.jcip.annotations.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private int capacity;
+
+    SimpleBlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void offer(T value) {
+        synchronized (value) {
+            queue.add(value);
+            capacity++;
+            this.notifyAll();
+        }
+        synchronized (this) {
+            while (queue.size() >= capacity) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public T poll() {
+        synchronized (Thread.currentThread()) {
+            try {
+                while (queue.size() == 0) {
+                    Thread.currentThread().wait();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        synchronized (this) {
+            if (queue.size() > 0) {
+                capacity--;
+                return queue.poll();
+            }
+        }
+        return null;
+    }
+}
