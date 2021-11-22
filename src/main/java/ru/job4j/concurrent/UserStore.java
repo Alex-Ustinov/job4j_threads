b02c9000f3894d29cf927f3d8379fbdd29d4a0be
18485888 <AAlUstinov@sberbank.ru>
2021-11-22 10:53:16 +0300

    Класс хранилища пользователей UserStorage

--- src/main/java/ru/job4j/concurrent/UserStore.java
+++ src/main/java/ru/job4j/concurrent/UserStore.java
@@ -10,36 +10,28 @@ public class UserStore {
     @GuardedBy("this")
     private Map<Integer, User> storage = new HashMap<>();
 
-    public synchronized boolean add (User user) {
-        User result = storage.putIfAbsent(user.getId(), user);
-        return checkIdUser(user, result);
+    public synchronized User add (User user) {
+        return storage.putIfAbsent(user.getId(), user);
     }
-    public synchronized boolean update(User user) {
-        User result = storage.replace(user.getId(), user);
-        return checkIdUser(user, result);
+    public synchronized User update(User user) {
+        return storage.replace(user.getId(), user);
     }
 
-    public synchronized boolean delete(User user) {
-        User result = storage.remove(user.getId());
-        return checkIdUser(user, result);
+    public synchronized User delete(User user) {
+        return storage.remove(user.getId());
     }
 
-    public boolean checkIdUser(User initialUser, User receiveUser) {
-        return initialUser.getId() == receiveUser.getId();
-    }
-
-    public void transfer(int fromId, int toId, int amount) throws Exception {
+    public synchronized void transfer(int fromId, int toId, int amount) throws Exception {
         User fromUser = storage.get(fromId);
         User toUser = storage.get(fromId);
-            synchronized (fromUser) {
-                synchronized (toUser) {
-                    if (fromUser.getAmount() <= 0 || fromUser.getAmount() - amount < 0) {
-                        throw new Exception("User does not have enough money");
-                    }
-                    toUser.setAmount(toUser.getAmount() + amount);
-                    fromUser.setAmount(fromUser.getAmount() - amount);
-
-                }
+        if (fromUser != null && toUser != null) {
+            if (fromUser.getAmount() <= 0 || fromUser.getAmount() - amount < 0) {
+                throw new Exception("User does not have enough money");
             }
+            if (fromUser.getAmount() - amount >= 0) {
+                toUser.setAmount(toUser.getAmount() + amount);
+                fromUser.setAmount(fromUser.getAmount() - amount);
+            }
+        }
     }
 }
