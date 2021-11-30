a152a007d4acb057d4d584fce8fcad9048e99ac3
18485888 <AAlUstinov@sberbank.ru>
2021-11-30 23:35:26 +0300

    CAS - операции

--- src/main/java/ru/job4j/thread/CASCount.java
+++ src/main/java/ru/job4j/thread/CASCount.java
@@ -10,17 +10,15 @@ public class CASCount {
 
     public void increment() {
         Integer countInMoment = count.get();
-        if (countInMoment == 0) {
-            throw new UnsupportedOperationException("Count is not impl.");
-        }
-        count.compareAndSet(countInMoment, count.get() + 1);
+        do {
+            if (countInMoment == 0) {
+                throw new UnsupportedOperationException("Count is not impl.");
+            }
+            count.getAndSet(countInMoment + 1);
+        } while (count.get() == countInMoment + 1);
     }
 
     public int get() {
-        Integer rsl = count.get();
-        if (rsl == 0) {
-            throw new UnsupportedOperationException("Count is not impl.");
-        }
-        return rsl;
+        return  count.get();
     }
 }
