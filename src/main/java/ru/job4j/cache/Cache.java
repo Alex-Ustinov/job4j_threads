f8fbcf0048fe44744bbb09c5eaa23767d538e921
18485888 <AAlUstinov@sberbank.ru>
2021-12-01 00:53:26 +0300

    Неблокирующий кеш

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,29 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        if (!memory.containsKey(model.getId())) {
+            return false;
+        }
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        do {
+            memory.put(model.getId(), new Base(model.getId(), model.getVersion() + 1));
+        } while (model.getVersion() + 1 == memory.get(model.getId()).getVersion());
+    }
+
+    public void  delete(Base model) {
+        memory.computeIfPresent(model.getId(), (key, val) -> memory.remove(key));
+    }
+}
