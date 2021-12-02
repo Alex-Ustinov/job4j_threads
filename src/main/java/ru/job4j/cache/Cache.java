7670b8c02aa4e817771718fd5783f7b7741184ef
18485888 <AAlUstinov@sberbank.ru>
2021-12-02 15:32:05 +0300

    Неблокирующий кеш

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -1,5 +1,6 @@
 package ru.job4j.cache;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,12 +20,31 @@ public class Cache {
             throw new OptimisticException("Versions are not equal");
         }
         do {
-            memory.put(model.getId(), new Base(model.getId(), model.getVersion() + 1));
+            memory.putIfAbsent(model.getId(), new Base(model.getId(), model.getVersion() + 1));
         } while (model.getVersion() + 1 == memory.get(model.getId()).getVersion());
         return true;
     }
 
     public void  delete(Base model) {
         memory.computeIfPresent(model.getId(), (key, val) -> memory.remove(key));
+    }
+    public static void main(String[] args) {
+        Map<Integer, Base> map = new HashMap<>();
+        Base base = new Base(1, 0);
+        map.put(base.getId(), base);
+
+
+        Base user1 = map.get(base.getId());
+        user1.setName("User 1");
+
+        Base user2 = map.get(base.getId());
+        user1.setName("User 2");
+
+        map.put(user1.getId(), user1);
+        map.put(user2.getId(), user2);
+        for (Map.Entry<Integer, Base> t : map.entrySet()) {
+            System.out.println(t.getValue().getName());
+        }
+
     }
 }
