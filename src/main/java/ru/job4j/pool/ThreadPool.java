ca45bb25be568b338bb8030a3a29aa01ce83c12a
18485888 <AAlUstinov@sberbank.ru>
2021-12-08 01:09:37 +0300

    ThreadPool

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,50 @@
+package ru.job4j.pool;
+
+import ru.job4j.thread.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(5);
+
+    private ThreadPool() {
+        initialization();
+    }
+
+    private void initialization() {
+        Integer size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        tasks.poll().run();
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+            threads.add(thread);
+        }
+    }
+
+    public void work(Runnable job) {
+        for (Thread thread : threads) {
+            try {
+                while (!thread.isInterrupted()) {
+                    tasks.offer(job);
+                }
+            } catch (InterruptedException e) {
+                thread.interrupt();
+            }
+        }
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+}
