daa8685065ba7b4da15fbb4740f699022a931944
18485888 <AAlUstinov@sberbank.ru>
2021-12-21 01:42:50 +0300

    ForkJoinPool

--- src/main/java/ru/job4j/pool/ParallelSearch.java
+++ src/main/java/ru/job4j/pool/ParallelSearch.java
@@ -1,52 +1,40 @@
 package ru.job4j.pool;
 
-import java.util.Arrays;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelSearch<T> extends RecursiveTask<Integer> {
 
-    private T[] users;
-    private T user;
-    private SearchIndex searchIndex;
+    private T[] items;
+    private T item;
+    private Integer searchIndexFrom;
+    private Integer searchIndexTo;
 
-    ParallelSearch(T[] users, T user, SearchIndex searchIndex) {
-        this.users = users;
-        this.user = user;
-        this.searchIndex = searchIndex;
+    ParallelSearch(T[] items, T item, Integer searchIndexFrom, Integer searchIndexTo) {
+        this.items = items;
+        this.item = item;
+        this.searchIndexFrom = searchIndexFrom;
+        this.searchIndexTo = searchIndexTo;
     }
 
     @Override
     protected Integer compute() {
-        if (users.length <= 10) {
-            for (int i = 0; i < users.length; i++) {
-                if (users[i] == user) {
-                    return i;
+        Integer result = -1;
+        if (searchIndexTo - searchIndexFrom <= 10) {
+            for (int i = searchIndexFrom; i < searchIndexTo; i++) {
+                if (items[i] == item) {
+                    result =  i;
                 }
             }
-        }
-
-        int middle =  Math.round(users.length / 2);
-
-        T[] firstPartSearch = Arrays.copyOfRange(users, 0, middle);
-
-        T[] rowBack = (T[])(new Object[users.length - 1 - middle]);
-        for (int i  = users.length - 1;  middle <= i; i--) {
-            rowBack[i] = users[i];
-        }
-        ParallelSearch parallelSearch1 = new ParallelSearch(firstPartSearch, user, new SearchIndex(user));
-        ParallelSearch parallelSearch2 = new ParallelSearch(rowBack, user, new SearchIndex(user));
-        parallelSearch1.fork();
-        parallelSearch2.fork();
-
-        parallelSearch1.join();
-        parallelSearch1.join();
-
-        int rsl1 = searchIndex.search(firstPartSearch);
-        int rsl2 = searchIndex.search(rowBack);
-        if(rsl1 > 0) {
-            return rsl1;
         } else {
-           return  rsl2;
+            Integer midl = (searchIndexFrom - searchIndexTo) / 2;
+            ParallelSearch parallelSearchFrom = new ParallelSearch(items, item, searchIndexFrom, midl);
+            ParallelSearch parallelSearchTo = new ParallelSearch(items, item, midl + 1, searchIndexTo);
+            parallelSearchFrom.fork();
+            parallelSearchTo.fork();
+
+            parallelSearchFrom.join();
+            parallelSearchTo.join();
         }
+        return result;
     }
 }
